d8341b349d96eeff52f66f4aa9cf0e3e874468c8
Endrew_Carlos <endrewcarlos2006@gmail.com>
2025-03-30 03:08:45 +0000

    Desafio das CartasSuperTrunfo nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-    // Declaração das variáveis - CARTA 01
     int main (){
+        
+         // Declaração das variáveis - CARTA 01
         int populacao, pontosturisticos;
         char estado;
         char codigo[5];
         char cidade[26];
         float area;
         float pib;
+        float densidadepopulacional;
+        float pibpercapita;
         
-        // Entrada das informações fornecidas pelo usuário    
+        // Entrada das informações fornecidas pelo usuário CARTA 1
         printf("CARTA 1\n");
         printf("Digite o nome do estado: \n");
         scanf("%c", &estado);
@@ -32,21 +35,28 @@
         printf("Digite o PIB: \n");
         scanf("%f", &pib);
 
-        // Saída das informações fornecidas pelo usuário
+        // Cálculo da Densidade Populacional e do PIB per Capita CARTA 1
+        densidadepopulacional = populacao / area;
+        pibpercapita = pib / populacao;
+
+        // Saída das informações fornecidas pelo usuário CARTA 1
         printf(" - A CARTA 1- \n - Estado: %c\n - Código da Carta: %s\n - Nome da cidade: %s\n", estado, codigo, cidade);
         printf(" - População: %d\n - Número de Pontos Turísticos: %d\n - área: %.2f KM²\n - PIB: %.2f Bilhões de Reais\n", populacao, pontosturisticos, area, pib);
-
-     // Declaração das variáveis - CARTA 02    
+        printf(" - Densidade Populacional: %.2f hab/km²\n", densidadepopulacional);
+        printf(" - PIB per Capita: %f REAIS\n", pibpercapita);
+        
+         // Declaração das variáveis - CARTA 02
         int populacao2, pontosturisticos2;
         char estado2;
         char codigo2[5];
         char cidade2[26];
         float area2;
         float pib2;
+        float densidadepopulacional2;
+        float pibpercapita2;
         
-     
-        // Entrada das informações fornecidas pelo usuário da - Carta 02
-        printf("CARTA 2\n");
+        // Entrada das informações fornecidas pelo usuário CARTA 2
+        printf("\n - CARTA 2 - \n");
         printf("Estado: B \n");
             
         printf("Digite o código da carta: \n");
@@ -67,9 +77,15 @@
         printf("Digite o PIB: \n");
         scanf("%f", &pib2);
     
-        // Saída das informações fornecidas pelo usuário da - Carta 02
+        // Cálculo da Densidade Populacional e do PIB per Capita CARTA 2
+        densidadepopulacional2 = populacao2 / area2;
+        pibpercapita2 = pib2 / populacao2;
+        
+        // Saída das informações fornecidas pelo usuário DA CARTA 2
         printf(" --A CARTA 2-- \n - Estado: B%c\n - Código da Carta: %s\n - Nome da cidade: %s\n", estado2, codigo2, cidade2);
         printf(" - População: %d\n - Número de Pontos Turísticos: %d\n - área: %.2f KM²\n - PIB: %.2f Bilhões de Reais\n", populacao2, pontosturisticos2, area2, pib2);
-
-            return 0;
+        printf(" - Densidade Populacional: %.2f hab/km²\n", densidadepopulacional2);
+        printf(" - PIB per Capita: %f REAIS\n", pibpercapita2);
+            
+        return 0;
         }
